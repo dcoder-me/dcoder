7f0578ad4860f6ea22f04240d4f12b4e98460890
Darpan <darpan@favfly.co>
2026-04-03 01:38:00 +0530

    update nav padding

--- components/nav.jsx
+++ components/nav.jsx
@@ -87,7 +87,7 @@ export const Nav = () => {
             : "bg-transparent"
         }`}
       >
-        <nav className="mx-auto flex max-w-6xl items-center justify-between px-6 py-4">
+        <nav className="mx-auto flex max-w-6xl items-center justify-between px-6 py-2 sm:py-4">
           {/* Logo */}
           <a
             href="#"
